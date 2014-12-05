09e82d48691a9546051f7af8b42ce5d40d184f3e
Jason Merrill <jwmerrill@gmail.com>
2014-12-05 14:31:26 -0800

    Update example to use both `refresh` and `refreshWhen`.

--- examples/counter.elm
+++ examples/counter.elm
@@ -1,5 +1,14 @@
 import Monitor
 import Signal
 import Text
+import Mouse
+import Graphics.Element (flow, down)
 
-main = Signal.map Text.asText (Signal.foldp (+) 0 Monitor.refresh)
+display counter1 counter2 = flow down
+    [ Text.asText counter1
+    , Text.asText counter2
+    ]
+
+main = Signal.map2 display
+    (Signal.foldp (+) 0 Monitor.refresh)
+    (Signal.foldp (+) 0 (Monitor.refreshWhen Mouse.isDown))
